8e5475238b7706d5f5f0633bd95160b9a6ba920e
gerlix-server <gerlixtechnology@mail.com>
2022-07-07 22:11:52 -0500

    Actualización de Api Flutter

--- lib/src/api/evironment.dart
+++ lib/src/api/evironment.dart
@@ -1,6 +1,6 @@
 class Environment{
 
   //static const String API_DELIVERY = "192.168.3.23:3000";
-  static const String API_DELIVERY = "https://github.com/GerlixTechnology/Gerlix-Server.git:3000";
+  static const String API_DELIVERY = "181.197.97.159:3000";
 
 }
